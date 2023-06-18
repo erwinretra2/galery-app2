1e373722239b9c20af952e736fda7e0032631192
Erwin947 <erwin20002@mail.unpad.ac.id>
2023-06-19 00:33:01 +0700

    Erwin

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Erwin Renanda Saputra" // TODO: replace this
+const studentId = "FE5161534" // TODO: replace this
+
+export { url, studentName, studentId }
