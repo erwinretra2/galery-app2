54ef635cdf59e5b389762cf7627330814bba8d86
Erwin947 <erwin20002@mail.unpad.ac.id>
2023-06-19 11:37:57 +0700

    add redirect

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "incredible-axolotl-374eb6.netlify.app" // TODO: replace this
 const studentName = "Erwin Renanda Saputra" // TODO: replace this
 const studentId = "FE5161534" // TODO: replace this
 
